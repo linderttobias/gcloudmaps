3d06dca432d21d402776a587a313787737a82521
tobias <linderttobias@gmail.com>
2023-04-29 15:58:04 +0200

    gcm-update-about-button

--- infrastructure/deployment.tf
+++ infrastructure/deployment.tf
@@ -1,6 +1,6 @@
 resource "google_cloudbuild_trigger" "frontend-prod" {
   location = var.region
-  name     = "build-deploy-gcloudmaps-prod"
+  name     = "build-deploy-frontend-gcloudmaps-prod"
   service_account = google_service_account.umsa-cloudbuild.id
   filename = "deployment/frontend.yaml"
 
@@ -11,5 +11,23 @@ resource "google_cloudbuild_trigger" "frontend-prod" {
       branch = "^main$"
     }
   }
+  
+  included_files = ["application/frontend"]
+}
 
+resource "google_cloudbuild_trigger" "backend-prod" {
+  location = var.region
+  name     = "build-deploy-backend-gcloudmaps-prod"
+  service_account = google_service_account.umsa-cloudbuild.id
+  filename = "deployment/backend.yaml"
+
+  github {
+    owner = "hunderttausendwatt"
+    name  = "gcloudmaps"
+    push {
+      branch = "^main$"
+    }
+  }
+  
+  included_files = ["application/backend"]
 }
