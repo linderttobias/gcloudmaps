25a60242273c1d17b40918c009a8ee52537537aa
tobias <linderttobias@gmail.com>
2023-04-29 12:52:30 +0200

    gcm-change-frontend-and-deployment

--- application/frontend/src/index.js
+++ application/frontend/src/index.js
@@ -2,11 +2,27 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
+import Home from "./Home";
+
+
+
+export default function Apps() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<App />} />
+          <Route path="/about" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Apps />
   </StrictMode>
 );
