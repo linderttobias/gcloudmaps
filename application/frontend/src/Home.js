dd76452155eb09cf69bc32234da4654597d42430
tobias <linderttobias@gmail.com>
2023-05-02 21:37:49 +0200

    gcm-update-various

--- application/frontend/src/Home.js
+++ application/frontend/src/Home.js
@@ -4,8 +4,8 @@ import './index.css';
 
 const Home = () => {
     useEffect(() => {
-        document.body.style.overflow = "hidden";
-        return () => (document.body.style.overflow = "scroll");
+        //document.body.style.overflow = "hidden";
+        //return () => (document.body.style.overflow = "scroll");
     });
 
     return (
@@ -20,17 +20,14 @@ const Home = () => {
             </div>
         
         <div className="info-content">
-                <div className="info-text">
-                  <h3> About Gcloudmaps:</h3>
-                  <p>more info coming soon ..
-                  </p>
-                  <h3> About Me:</h3>
-                  <p>Hey there! I'm a Cloud Data Engineer with a passion for tackling complex challenges through the power of cloud technologies. In the rare event of a global network apocalypse, you'll catch me shredding up fresh powder in the Alps, kitesurfing across my local lake, or whipping up a scrumptious eggplant lasagna for my friends to enjoy as we ride out the digital storm.
-                  </p>
-                </div>
                 <div className="info-image">
                   <h3>Architecture:</h3>
                   <img src="images/architecture.svg" alt="info image" />
+                </div>
+                <div className="info-text">
+                <h3> About Me:</h3>
+                  <p>Hey there! I'm a Cloud Data Engineer with a passion for tackling complex challenges through the power of cloud technologies. In the rare event of a global network apocalypse, you'll catch me shredding up fresh powder in the Alps, kitesurfing across my local lake, or whipping up a scrumptious eggplant lasagna for my friends to enjoy as we ride out the digital storm.
+                  </p>
                 </div>
         </div>
         </div>
