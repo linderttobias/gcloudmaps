e237eac495fa20e5b11236955b6002cda7631151
tobias <linderttobias@gmail.com>
2023-09-24 17:07:47 +0200

    ci: update and remove files for pipelines

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -40,31 +40,24 @@ resource "google_artifact_registry_repository_iam_member" "member" {
 # act as the Runtime Service Account of your Cloud Run service.
 
 
-resource "google_service_account" "umsa-cloudbuild" {
-  account_id   = "gcloudmaps-cloudbuild-umsa"
-  display_name = "User-managed Service Account for the Cloud Build"
+resource "google_service_account" "umsa-deployment" {
+  account_id   = "deployment-github-umsa"
+  display_name = "User-managed Service Account for the deployment with github actions"
 }
 
-
+# Required to Deployment Pipeline can assign Cloud Run SA to Service
 resource "google_service_account_iam_member" "user-account-iam" {
   service_account_id = google_service_account.umsa-cloudrun.name
   role               = "roles/iam.serviceAccountUser"
-  member             = "serviceAccount:${google_service_account.umsa-cloudbuild.email}"
-}
-
-
-resource "google_project_iam_member" "project" {
-  project = var.project
-  role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.umsa-cloudbuild.email}"
+  member             = "serviceAccount:${google_service_account.umsa-deployment.email}"
 }
 
 resource "google_artifact_registry_repository_iam_member" "member-cloudbuild" {
   project    = google_artifact_registry_repository.gcloudmaps.project
   location   = google_artifact_registry_repository.gcloudmaps.location
   repository = google_artifact_registry_repository.gcloudmaps.name
-  role       = "roles/artifactregistry.createOnPushWriter"
-  member     = "serviceAccount:${google_service_account.umsa-cloudbuild.email}"
+  role       = "roles/artifactregistry.writer"
+  member     = "serviceAccount:${google_service_account.umsa-deployment.email}"
 }
 
 
@@ -73,7 +66,7 @@ resource "google_cloud_run_service_iam_member" "member-frontend" {
   project  = var.project
   service  = google_cloud_run_v2_service.frontend.name
   role     = "roles/run.admin"
-  member   = "serviceAccount:${google_service_account.umsa-cloudbuild.email}"
+  member   = "serviceAccount:${google_service_account.umsa-deployment.email}"
 }
 
 resource "google_cloud_run_service_iam_member" "member-backend" {
@@ -81,5 +74,5 @@ resource "google_cloud_run_service_iam_member" "member-backend" {
   project  = var.project
   service  = google_cloud_run_v2_service.backend.name
   role     = "roles/run.admin"
-  member   = "serviceAccount:${google_service_account.umsa-cloudbuild.email}"
+  member   = "serviceAccount:${google_service_account.umsa-deployment.email}"
 }
