848ae0a1c6a11799bf7642929d471c9b816cf331
tobias <linderttobias@gmail.com>
2023-09-24 20:54:00 +0200

    chore: format tf code

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,22 +1,22 @@
 resource "google_service_account" "umsa-cloudrun" {
-  account_id   = "gcloudmaps-cloudrun-umsa"
+  account_id  = "gcloudmaps-cloudrun-umsa"
   description = "user-managed Service Account to run Cloud Run services"
 }
 
 resource "google_service_account" "umsa-deployment" {
-  account_id   = "deployment-github-umsa"
+  account_id  = "deployment-github-umsa"
   description = "user-managed Service Account for the build and deployment (backend and frontend) with github actions"
 }
 
 # UMSA for Terraform. Roles are added manually
 resource "google_service_account" "umsa-tf-plan" {
-  account_id   = "tf-plan-umsa"
+  account_id  = "tf-plan-umsa"
   description = "Service Account for Terraform Plan on GitHub Actions"
 }
 
 # UMSA for Terraform. Roles are added manually
 resource "google_service_account" "umsa-tf-apply" {
-  account_id   = "tf-apply-umsa"
+  account_id  = "tf-apply-umsa"
   description = "Service Account for Terraform Apply on GitHub Actions"
 }
 
@@ -48,19 +48,13 @@ resource "google_artifact_registry_repository_iam_member" "member" {
   member     = "serviceAccount:${google_service_account.umsa-cloudrun.email}"
 }
 
-
-# ForCloud Build
-# User (roles/iam.serviceAccountUser)
-#Logs Writer (roles/logging.logWriter) role
-#Artifact Registry Create-on-push Writer (roles/artifactregistry.createOnPushWriter) role 
-# act as the Runtime Service Account of your Cloud Run service.
-
 # Required to Deployment Pipeline can assign Cloud Run SA to Service
 resource "google_service_account_iam_member" "user-account-iam" {
   service_account_id = google_service_account.umsa-cloudrun.name
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${google_service_account.umsa-deployment.email}"
 }
+
 
 resource "google_artifact_registry_repository_iam_member" "member-cloudbuild" {
   project    = google_artifact_registry_repository.gcloudmaps.project
