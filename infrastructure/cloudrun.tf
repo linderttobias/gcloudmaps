018022beb82443115fd31de1040a7b854cb4c6ca
lindertobias <linderttobias@gmail.com>
2023-10-27 21:29:11 +0200

    ops: add testing deployment

--- infrastructure/cloudrun.tf
+++ infrastructure/cloudrun.tf
@@ -61,3 +61,63 @@ resource "google_cloud_run_v2_service" "backend" {
   }
 
 }
+
+
+resource "google_cloud_run_v2_service" "frontend-test" {
+  name     = "gcloudmaps-frontend-test"
+  location = var.region
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
+
+
+  template {
+
+    labels = var.labels
+
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 1
+    }
+
+    service_account = google_service_account.umsa-cloudrun.email
+
+    containers {
+      image = "europe-west1-docker.pkg.dev/gcloudmaps/gcloudmaps/frontend"
+      ports {
+        container_port = 3000
+      }
+
+       env {
+        name = "FOO"
+        value = "bar"
+      }
+    }
+  }
+
+}
+
+resource "google_cloud_run_v2_service" "backend-test" {
+  name     = "gcloudmaps-backend-test"
+  location = var.region
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
+
+
+  template {
+
+    labels = var.labels
+
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 1
+    }
+
+    service_account = google_service_account.umsa-cloudrun.email
+
+    containers {
+      image = "europe-west1-docker.pkg.dev/gcloudmaps/gcloudmaps/backend"
+      ports {
+        container_port = 3001
+      }
+    }
+  }
+
+}
