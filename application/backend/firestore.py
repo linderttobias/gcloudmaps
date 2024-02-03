f5cd234384c92dde7b03dd25911f4179230bb073
lindertobias <linderttobias@gmail.com>
2024-02-03 10:55:10 +0100

    chore: remove hard-coded google project

--- application/backend/firestore.py
+++ application/backend/firestore.py
@@ -1,9 +1,11 @@
+import os
 from flask import current_app
 from google.cloud import firestore
+
 # The `project` parameter is optional and represents which project the client
 # will act on behalf of. If not supplied, the client falls back to the default
 # project inferred from the environment.
-db = firestore.Client(project="experimentplanet")
+db = firestore.Client()
 
 ## If user is existing, returns the document, in this case ['bigquery', 'cloudrun']
 def load_mindmaps_list(user: str):
